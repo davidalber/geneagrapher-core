9380050fd31e7e9f3d611e12ba9339d7003c5fd7
David Alber <alber.david@gmail.com>
2023-01-29 22:51:37 -0800

    Add function to get complete record
    This also adds stub functions to get the actual record details.

--- geneagrapher_core/record.py
+++ geneagrapher_core/record.py
@@ -1,8 +1,32 @@
 from bs4 import BeautifulSoup
-from typing import NewType
+from typing import List, NewType, Optional, TypedDict
 import urllib.request
 
 RecordId = NewType("RecordId", int)
+
+
+class Record(TypedDict):
+    id: RecordId
+    name: str
+    institution: Optional[str]
+    year: Optional[int]
+    descendants: List[int]
+    ancestors: List[int]
+
+
+def get_record(record_id: RecordId) -> Record:
+    soup: BeautifulSoup = fetch_document(record_id)
+
+    result: Record = {
+        "id": record_id,
+        "name": get_name(soup),
+        "institution": get_institution(soup),
+        "year": get_year(soup),
+        "descendants": get_descendants(soup),
+        "ancestors": get_ancestors(soup),
+    }
+
+    return result
 
 
 def fetch_document(rid: RecordId) -> BeautifulSoup:
@@ -10,3 +34,23 @@ def fetch_document(rid: RecordId) -> BeautifulSoup:
         f"https://www.mathgenealogy.org/id.php?id={rid}"
     ) as page:
         return BeautifulSoup(page, "html.parser")
+
+
+def get_name(soup: BeautifulSoup) -> str:
+    return ""
+
+
+def get_institution(soup: BeautifulSoup) -> Optional[str]:
+    ...
+
+
+def get_year(soup: BeautifulSoup) -> Optional[int]:
+    ...
+
+
+def get_descendants(soup: BeautifulSoup) -> List[int]:
+    return []
+
+
+def get_ancestors(soup: BeautifulSoup) -> List[int]:
+    return []
--- tests/test_record.py
+++ tests/test_record.py
@@ -1,5 +1,38 @@
-from geneagrapher_core.record import fetch_document
+from geneagrapher_core.record import fetch_document, get_record
 from unittest.mock import MagicMock, patch, sentinel as s
+
+
+@patch("geneagrapher_core.record.get_ancestors")
+@patch("geneagrapher_core.record.get_descendants")
+@patch("geneagrapher_core.record.get_year")
+@patch("geneagrapher_core.record.get_institution")
+@patch("geneagrapher_core.record.get_name")
+@patch("geneagrapher_core.record.fetch_document")
+def test_get_record(
+    m_fetch_document,
+    m_get_name,
+    m_get_institution,
+    m_get_year,
+    m_get_descendants,
+    m_get_ancestors,
+) -> None:
+    m_soup = m_fetch_document.return_value
+
+    assert get_record(s.rid) == {
+        "id": s.rid,
+        "name": m_get_name.return_value,
+        "institution": m_get_institution.return_value,
+        "year": m_get_year.return_value,
+        "descendants": m_get_descendants.return_value,
+        "ancestors": m_get_ancestors.return_value,
+    }
+
+    m_fetch_document.assert_called_once_with(s.rid)
+    m_get_name.assert_called_once_with(m_soup)
+    m_get_institution.assert_called_once_with(m_soup)
+    m_get_year.assert_called_once_with(m_soup)
+    m_get_descendants.assert_called_once_with(m_soup)
+    m_get_ancestors.assert_called_once_with(m_soup)
 
 
 @patch("geneagrapher_core.record.BeautifulSoup")
